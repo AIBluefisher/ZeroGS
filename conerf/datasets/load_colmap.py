[FIX] Fix the bug that the coordinate frame of point cloud does not match with camera poses when scaling scenes

conerf/datasets/load_colmap.py
@@ -110,6 +110,7 @@ def load_colmap(
     manager = SceneManager(colmap_dir, load_points=True)
     manager.load()
 
+    points3d, colors = manager.points3D, manager.point3D_colors
     colmap_image_data = manager.images
     colmap_camera_data = manager.cameras
     image_name_to_image_id = manager.name_to_image_id
@@ -161,7 +162,7 @@ def load_colmap(
         camtoworlds = np.einsum("nij, ki -> nkj", camtoworlds, T)
         camtoworlds[:, :3, 3:4] *= scale
 
-        points3d = scale * (T[:3, :3] @ manager.points3D.T + T[:3, 3][..., None]).T # [Np, 3]
+        points3d = scale * (T[:3, :3] @ points3d.T + T[:3, 3][..., None]).T # [Np, 3]
 
         if rotate:
             # Rotate the scene to align with ground plane.
@@ -174,7 +175,6 @@ def load_colmap(
             camtoworlds = camtoworlds.numpy()
 
     if not os.path.exists(ply_path):
-        points3d, colors = manager.points3D, manager.point3D_colors
         print("Converting point3d.bin to .ply " +
               "will happen only the first time you open the scene.")
         store_ply(ply_path, points3d, colors)